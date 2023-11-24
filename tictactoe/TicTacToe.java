c2e6cdcc55f28f0eb0f1046a765b35f44a4a366d
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-11-23 19:02:15 -0500

    Updated the gui and implemented three extra game modes where the user can play against the computer

--- tictactoe/TicTacToe.java
+++ tictactoe/TicTacToe.java
@@ -11,9 +11,14 @@ public class TicTacToe extends JFrame {
     private static final int GAP = 5;
     protected static final Font LABEL_RESET_FONT = new Font("Courier", Font.BOLD, 11);
     protected static GameMode gameMode = GameMode.GAME_NOT_STARTED;
-    private PlayingField playingField = new PlayingField();
     protected static JLabel statusLabel = new JLabel();
-    protected static JButton resetButton = new JButton();
+    protected static StatusStartReset startOrReset = StatusStartReset.START;
+    protected static PlayerType player1 = PlayerType.HUMAN;
+    protected static PlayerType player2 = PlayerType.HUMAN;
+    protected static JButton startResetButton = new JButton();
+    protected static JButton player1Button = new JButton();
+    protected static JButton player2Button = new JButton();
+    private PlayingField playingField = new PlayingField();
     public TicTacToe() {
         super("Tic Tac Toe");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -24,20 +29,35 @@ public class TicTacToe extends JFrame {
         // Add components to the frame
         add(playingField, BorderLayout.CENTER);
         setBottomPanel();
+        setUpperPanel();
 
         setVisible(true);
     }
 
+    private void setUpperPanel() {
+        JPanel upperPanel = new JPanel();
+        upperPanel.setBorder(BorderFactory.createEmptyBorder(GAP, GAP, 0, GAP));
+        upperPanel.setLayout(new GridLayout(1, 3, GAP * 2, GAP * 2));
+
+        setPlayer1Button();
+        upperPanel.add(player1Button);
+
+        setStartResetButton();
+        upperPanel.add(startResetButton);
+
+        setPlayer2Button();
+        upperPanel.add(player2Button);
+
+        add(upperPanel, BorderLayout.NORTH);
+    }
+
     private void setBottomPanel() {
         JPanel bottomPanel = new JPanel();
-        bottomPanel.setBorder(BorderFactory.createEmptyBorder(GAP, GAP, GAP, GAP));
+        bottomPanel.setBorder(BorderFactory.createEmptyBorder(0, GAP, GAP, GAP));
         bottomPanel.setLayout(new BorderLayout());
 
         setStatusLabel();
         bottomPanel.add(statusLabel, BorderLayout.WEST);
-
-        setResetButton();
-        bottomPanel.add(resetButton, BorderLayout.EAST);
 
         add(bottomPanel, BorderLayout.SOUTH);
     }
@@ -48,21 +68,109 @@ public class TicTacToe extends JFrame {
         statusLabel.setFont(LABEL_RESET_FONT);
     }
 
-    private void setResetButton() {
-        resetButton.setName("ButtonReset");
-        resetButton.setText("Reset");
-        resetButton.setFocusable(false);
-        resetButton.setFont(LABEL_RESET_FONT);
-        resetButton.setBackground(Color.BLACK);
-        resetButton.setForeground(UIManager.getColor ( "Panel.background" ));
-        resetButton.addActionListener(e -> {
-            Set<Cell> cells = Cell.getCells();
-            for (Cell cell: cells) {
-                cell.setText(" ");
+    private void setStartResetButton() {
+        startResetButton.setName("ButtonStartReset");
+        startResetButton.setText(startOrReset.getButtonLabel());
+        startResetButton.setFocusable(false);
+        startResetButton.setFont(LABEL_RESET_FONT);
+        startResetButton.setBackground(Color.BLACK);
+        startResetButton.setForeground(UIManager.getColor ( "Panel.background" ));
+        startResetButton.addActionListener(e -> {
+            if (startOrReset == StatusStartReset.RESET) {
+
+                Set<Cell> cells = Cell.getCells();
+                for (Cell cell: cells) {
+                    cell.setText(" ");
+                    cell.setEnabled(false);
+                }
+                Cell.setXTurn(true);
+
+                gameMode = GameMode.GAME_NOT_STARTED;
+                statusLabel.setText(gameMode.getTextLabel());
+
+                startOrReset = StatusStartReset.START;
+                startResetButton.setText(startOrReset.getButtonLabel());
+
+                player1Button.setEnabled(true);
+                player2Button.setEnabled(true);
+
+            } else if (startOrReset == StatusStartReset.START) {
+
+                gameMode = GameMode.GAME_IN_PROGRESS;
+                statusLabel.setText(gameMode.getTextLabel());
+
+                startOrReset = StatusStartReset.RESET;
+                startResetButton.setText(startOrReset.getButtonLabel());
+
+                Set<Cell> cells = Cell.getCells();
+                for (Cell cell: cells) {
+                    cell.setEnabled(true);
+                }
+
+                player1Button.setEnabled(false);
+                player2Button.setEnabled(false);
+
+                startGame();
             }
-            Cell.setXTurn(true);
-            gameMode = GameMode.GAME_NOT_STARTED;
-            statusLabel.setText(gameMode.getTextLabel());
+
         });
+    }
+
+    private void setPlayer1Button() {
+        player1Button.setName("ButtonPlayer1");
+        player1Button.setText(player1.getPlayerType());
+        player1Button.setFocusable(false);
+        player1Button.setFont(LABEL_RESET_FONT);
+        player1Button.setBackground(UIManager.getColor ( "Panel.background" ));
+        player1Button.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
+        player1Button.addActionListener(e -> {
+            if (startOrReset == StatusStartReset.START) {
+                player1 = player1 == PlayerType.HUMAN ? PlayerType.ROBOT : PlayerType.HUMAN;
+                player1Button.setText(player1.getPlayerType());
+            }
+        });
+    }
+
+    private void setPlayer2Button() {
+        player2Button.setName("ButtonPlayer2");
+        player2Button.setText(player2.getPlayerType());
+        player2Button.setFocusable(false);
+        player2Button.setFont(LABEL_RESET_FONT);
+        player2Button.setBackground(UIManager.getColor ( "Panel.background" ));
+        player2Button.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
+        player2Button.addActionListener(e -> {
+            if (startOrReset == StatusStartReset.START) {
+                player2 = player2 == PlayerType.HUMAN ? PlayerType.ROBOT : PlayerType.HUMAN;
+                player2Button.setText(player2.getPlayerType());
+            }
+        });
+    }
+
+    private void startGame() {
+        // TODO
+        if (player1 == PlayerType.ROBOT && player2 == PlayerType.ROBOT) {
+            while (gameMode == GameMode.GAME_IN_PROGRESS) {
+                Thread thread = new Thread(() -> {
+                    pressRandomCell();
+                });
+                thread.start();
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } else if (player1 == PlayerType.ROBOT && player2 == PlayerType.HUMAN) {
+            pressRandomCell();
+        }
+    }
+
+    protected static void pressRandomCell() {
+        for (Cell cell: Cell.getCells()) {
+            if (cell.getText().equals(" ")) {
+                cell.doClick();
+                return;
+            }
+        }
     }
 }
